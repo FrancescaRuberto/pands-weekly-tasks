18776efdd84204436931d708dad6a3110969c64c
Francesca Ruberto <francescaruberto01@gmail.com>
2024-02-12 23:32:06 +0000

    bank.py

--- bank.py
+++ bank.py
@@ -1,2 +1,14 @@
-###
+# This program promt the user and read in 2 money amounts
+# Author: Francesca Ruberto
+
+
+amount1 = int(input("Enter amount1 in cents: "))
+amount2 = int(input("Enter amount2 in cents: "))
+
+total = amount1 + amount2
+
+euros = total // 100
+cents = total % 100
+
+print(f"The sum of this is: €{euros}.{cents}" .format(euros, cents))
 
