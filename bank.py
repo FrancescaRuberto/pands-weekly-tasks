ce07d3263cb625b7cbdf71f250a4471a877083b6
Francesca Ruberto <francescaruberto01@gmail.com>
2024-04-07 22:48:54 +0100

    bank.py, adjusted program to calculate sum of two money amounts

--- bank.py
+++ bank.py
@@ -1,14 +1,20 @@
-# This program promt the user and read in 2 money amounts
+# This program prompts the user to input two amounts of money in cents.
+# It then calculates the sum of the two amounts and converts it into euros and cents.
 # Author: Francesca Ruberto
 
-
+# Prompt the user to enter the first amount in cents
 amount1 = int(input("Enter amount1 in cents: "))
+
+# Prompt the user to enter the second amount in cents
 amount2 = int(input("Enter amount2 in cents: "))
 
+# Calculate the total sum of the two amounts
 total = amount1 + amount2
 
+# Convert the total sum into euros and cents
 euros = total // 100
 cents = total % 100
 
-print(f"The sum of this is: €{euros}.{cents}" .format(euros, cents))
+# Print the sum in euros and cents
+print(f"The sum of the amounts is: €{euros}.{cents}")
 
