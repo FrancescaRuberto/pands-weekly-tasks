9e107178dbad78cea3dbd5496a84318bc5525112
Francesca Ruberto <francescaruberto01@gmail.com>
2024-04-12 11:04:50 +0100

    finalized program that plot an histogram

--- README.md
+++ README.md
@@ -98,3 +98,10 @@ Note1: This code snippet is designed to count the occurrences of the letter "e" 
 
 Note2: The solution to this task can be found in the program uploaded to this Git repository. 
 
+### Week 8 - plottask.py
+Description: This week, we delved into plotting using Matplotlib, a versatile Python library. Alongside, we introduced NumPy for efficient array manipulation, as Matplotlib often utilizes NumPy arrays for plotting. While we covered basic plotting techniques, both libraries offer extensive capabilities. Additionally, Seaborn, built on Matplotlib, provides further features for statistical data visualization. The project details for assessment are now available, marking the conclusion of weekly tasks. 
+
+Weekly task: 
+This week's task centered on Matplotlib plotting. While the histogram part felt familiar, the second task, plotting the function h(x) = x^3, posed a challenge. To understand the approach, I delved into NumPy documentation extensively. After some research, I found suitable options to accomplish the plot. Despite the initial difficulty, I managed to complete the task by keeping my programming simple and linear. The online sources I used included:
+-
+
--- plottask.py
+++ plottask.py
@@ -2,3 +2,40 @@
 #And a plot of the function  h(x)=x3 in the range 0 to 10
 
 #Author: Francesca Ruberto
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#We need to generate 1000 random values from a normal distribution with mean 5 and standard deviation 2
+values = np.random.normal(5, 2, 1000)
+
+#Now I create an histogram of the value
+plt.hist(values, bins=5, color='blue', edgecolor='black', label='Normal distribution')
+
+#Now let's define the function h(x) = x^3 in the range 0 to 10
+#This passage has been quite tricky for me. 
+#According to my researches on numpy documentation I have a couple of options available
+#The easier way should be using linspace, that allows us to generate evenly spaces numbers on a specified interval
+x = np.linspace(0, 10, 100)
+
+#Now that we have the x I need to generate the corresponding y value for this funciton
+y = x ** 3
+
+#Let's plot the function h(x) = x^3
+plt.plot(x, y, label='$h(x) = x^3$')
+
+#I need to add a title to this plot
+plt.title("Histogram of normal distribution and plot of h(x) = x^3 function")
+#Let's also add x and y axes labels
+plt.xlabel('Range')
+plt.ylabel('$h(x)$')
+
+#Let's add a legend
+plt.legend()
+
+#Let's display our plot
+plt.show()
+
+
+
+
