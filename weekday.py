aa112d9e4317ef15b3f13e33957ec1ce5259643b
Francesca Ruberto <francescaruberto01@gmail.com>
2024-04-12 11:13:09 +0100

    reviewed and modified program that outputs if today is or not a weekday

--- weekday.py
+++ weekday.py
@@ -1,22 +1,23 @@
 # This program outpuys whether or not today is a weekday
 # Auhor: Francesca Ruberto
 
-#First thing, I need to understand how to work out what day it is. 
-# Apparently I need to import datetime as first step
+# First, I need to understand how to determine the current day.
+# It seems I need to import the datetime module as the first step.
 
 import datetime
 
-# Once imported datetime I should specify the current date
-# In this way the program at the end should understand which day it is
+# Now that I've imported datetime, I have to specify the current date.
+# This allows the program to determine the current day of the week.
 today = datetime.date.today()
 
-# Now I need to "categorize" the days of week
-# So the program can distinguish between weekdays and weekend days
-# Since we count from 0, Monday should be 0 and Sunday 6
+# Next, I need to categorize the days of the week
+# So that the program can distinguish between weekdays and weekend days.
+# Since we count from 0, Monday should be 0 and Sunday should be 6.
 
 day_week = today.weekday()
 
-# Now I need the program to check which day is today
+# Finally, I need the program to check which day today is
+# and print the appropriate message.
 if day_week < 5:
     print("Yes, unfortunately today is weekday")
 else:
